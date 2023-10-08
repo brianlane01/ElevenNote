a1f25dc0acc67ae2361350d2a8acbbab7f1df348
brianlane01 <brian.storm.lane@icloud.com>
2023-10-07 23:16:12 -0400

    Added Dependency Injection

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,4 +1,23 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserEntity>(options =>
+{
+    options.Password.RequiredLength =4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole<int>>() 
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 
