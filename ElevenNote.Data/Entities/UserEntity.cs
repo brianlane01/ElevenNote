ed3302863363d300a63b1de8ec3e9dcf36b32a70
brianlane01 <brian.storm.lane@icloud.com>
2023-10-07 22:49:15 -0400

    Added Classes

---
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+using System.Componentmodel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ElevenNote.Data.Entities
+{
+    public class UserEntity : IdentityUser<int>
+    {
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
